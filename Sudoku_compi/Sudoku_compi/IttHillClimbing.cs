ecee03531990173bed25d44001c5e14588c324f1
Storm <stormluykx@gmail.com>
2024-12-10 18:20:35 +0100

    First working version

--- Sudoku_compi/Sudoku_compi/IttHillClimbing.cs
+++ Sudoku_compi/Sudoku_compi/IttHillClimbing.cs
@@ -2,59 +2,94 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
-using System.ComponentModel;
 using System.Numerics;
+using System.Diagnostics;
 
 namespace Sudoku_compi 
 {
     public class IttHillClimbing
     {
-        Board board;
-        static Random rnd = new Random();
+        // Run stats
+        public Stopwatch SW;
+        public TimeSpan Elapsed;
+        public int AttemptsNeeded;
+        public int TotalSwaps = 0;
+
+        // Algo settings
+        private int MaxAttempts = 100;
+        private int OptimumCeiling = 100;
+        private int RandomSwaps = 20;
+
+        // Algo board
+        public Board Board;
+        public Random rnd = new Random();
+
         public IttHillClimbing(Board sBoard) 
         {
-            board = sBoard;
-            ILS();
+            Board = sBoard;
         }
 
-        private void ILS()
+        public void Run()
         {
-            //First swap til optimum is reached
-            swapTilOptimum(1000, board);
-            //Then RandomWalk
-            if (board.boardHScore != 0)
-            {
-                //Randomwalkfunction
+            SW = new Stopwatch();
+            SW.Start();
+
+            // Number of attempts to solve the Sudoku puzzle so far
+            // One attempt is swapping till an optimum once.
+            int attempts = 1;
+
+            while (attempts <= MaxAttempts) {
+                //First swap till optimum is reached
+                SwapTillOptimum(OptimumCeiling);
+
+                // Don't do random walk in last iteration (as it will just scramble the board)
+                if (attempts == MaxAttempts) {
+                    Console.WriteLine("Was not able to solve the board.");
+                    break;
+                }
+
+                // Do random walk if board has not yet been solved
+                if (Board.BoardHScore != 0)
+                {
+                    RandomWalk(RandomSwaps);
+                }
+                else
+                {
+                    AttemptsNeeded = attempts;
+                    Console.WriteLine("Succesfully solved!");
+                    break;
+                }
+                attempts++;
             }
+            SW.Stop();
+            Elapsed = SW.Elapsed;
         }
 
-        public void swapTilOptimum(int ceiling, Board board) //ceiling determines how many long the swapping will continue without a score delta above 0
+        private void SwapTillOptimum(int ceiling) //ceiling determines how many long the swapping will continue without a score delta above 0
         {
             int count = 0;
 
             while (count < ceiling)
             {
-                if (board.boardHScore == 0) { break; }
+                if (Board.BoardHScore == 0) { break; }
                 //Select random 3x3 box
                 int vIndex = rnd.Next(0, 3);
                 int hIndex = rnd.Next(0, 3);
 
-                List<(Coord, Coord)> cc = board.getLegalSwaps((vIndex, hIndex));
+                List<(Coord, Coord)> cc = Board.GetLegalSwaps((vIndex, hIndex));
                 List<Swap> swaps = new List<Swap>();
 
                 foreach ((Coord, Coord) swap in cc)
                 {
                     //Transform to Swap objects
-                    swaps.Add(board.CoordsToSwap(swap.Item1, swap.Item2));
+                    swaps.Add(new Swap(swap.Item1,swap.Item2,Board));
                 }
 
                 int delta = -1;
                 //This list will hold the swap(s) with the highest delta
                 List<Swap> doSwaps = new();
 
-                foreach (Swap s in  swaps)
+                foreach (Swap s in swaps)
                 {
                     if (s.Score > delta)
                     {
@@ -75,18 +110,28 @@ namespace Sudoku_compi
                 {
                     int rIndex = rnd.Next(doSwaps.Count);
                     Swap s = doSwaps[rIndex];
-                    board.CommitSwap(s);
+                    Board.CommitSwap(s);
+                    TotalSwaps++;
                 }
 
                 //If there is an improvement, start count over
                 if (delta > 0) { count = 0; }
                 //If score stays the same, count 1 up
-                else if (delta == 0) { count++; }
-                //If the swaps only have negative delta, stop the while
-                else { count = ceiling; }
+                else { count++; }
             }
+        }
 
-
+        private void RandomWalk(int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                (int, int) box = (rnd.Next(3), rnd.Next(3));
+                List<(Coord, Coord)> legalSwaps = Board.GetLegalSwaps(box);
+                int randomIndex = rnd.Next(legalSwaps.Count);
+                Swap randomSwap = new Swap(legalSwaps[randomIndex].Item1, legalSwaps[randomIndex].Item2, Board);
+                Board.CommitSwap(randomSwap);
+                TotalSwaps++;
+            }
         }
     }
 }
--- Sudoku_compi/Sudoku_compi/Swap.cs
+++ Sudoku_compi/Sudoku_compi/Swap.cs
@@ -27,10 +27,88 @@ namespace Sudoku_compi
         // The new heuristic value of the second affected column if the swap is committed
         public int newHValCol2;
 
-        public Swap(Coord coord1, Coord coord2)
+        public Swap(Coord coord1, Coord coord2, Board b)
         {
             Coord1 = coord1;
             Coord2 = coord2;
+
+            int totalDelta = 0;
+
+            newValCoord1 = b.board[coord2.X,coord2.Y];
+            newValCoord2 = b.board[coord1.X,coord1.Y];
+
+            // If the two coords share the same column
+            if (coord1.X == coord2.X)
+            {
+                // Value of column does not change so just set the new column values to the current value 
+                newHValCol1 = b.ColHVals[coord1.X];
+                newHValCol2 = b.ColHVals[coord1.X];
+            }
+            else
+            {
+                // In this column the value of coord1 is replaced by the value of coord2
+                int[] col1 = Enumerable.Range(0, 9)
+                    .Select(i => b.board[coord1.X, i])
+                    .ToArray();
+                
+                // Replace value of coord1 with value of coord2
+                col1[coord1.Y] = b.board[coord2.X, coord2.Y];
+                // Calculate new value of col1
+                int hValCol1 = b.lineHeuristic(col1);
+                // Calculate difference between old and new column value;
+                // formula = old - new; a positive value means the row has gotten closer to completion
+                int col1Delta = b.ColHVals[coord1.X] - hValCol1;
+                totalDelta += col1Delta;
+
+                newHValCol1 = hValCol1;
+
+                // In this row the value of coord2 is replaced with the value of coord1
+                int[] col2 = Enumerable.Range(0, 9)
+                    .Select(i => b.board[coord2.X, i])
+                    .ToArray();
+                
+                col2[coord2.Y] = b.board[coord1.X, coord1.Y];
+                int hValCol2 = b.lineHeuristic(col2);
+                int col2Delta = b.ColHVals[coord2.X] - hValCol2;
+                totalDelta += col2Delta;
+
+                newHValCol2 = hValCol2;
+            }
+
+            // If the two coords share the same row
+            if (coord1.Y == coord2.Y)
+            {
+                // Value of the row does not changeso just set the new row values to the current row value
+                newHValRow1 = b.RowHVals[coord1.Y];
+                newHValRow2 = b.RowHVals[coord1.Y];
+            }
+            else 
+            {
+                // In this row the value of coord1 is replaced with the value of coord2
+                int[] row1 = Enumerable.Range(0, 9)
+                    .Select(i => b.board[i, coord1.Y])
+                    .ToArray();
+                
+                row1[coord1.X] = b.board[coord2.X, coord2.Y];
+                int hValRow1 = b.lineHeuristic(row1);
+                int row1Delta = b.RowHVals[coord1.Y] - hValRow1;
+                totalDelta += row1Delta;
+
+                newHValRow1 = hValRow1;
+
+                // In this row the value of coord2 is replaced with the value of coord1
+                int[] row2 = Enumerable.Range(0, 9)
+                    .Select(i => b.board[i, coord2.Y])
+                    .ToArray();
+                
+                row2[coord2.X] = b.board[coord1.X, coord1.Y];
+                int hValRow2 = b.lineHeuristic(row2);
+                int row2Delta = b.RowHVals[coord2.Y] - hValRow2;
+                totalDelta += row2Delta;
+
+                newHValRow2 = hValRow2;
+            }
+            Score = totalDelta;
         }
     }
 
